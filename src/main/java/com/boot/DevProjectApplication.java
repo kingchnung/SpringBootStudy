3be5700a4e35b6bd79f516b894208cc00933b3f6
cksdnd1232@gmail.com <cksdnd1232@gmail.com>
2025-08-12 20:49:02 +0900

    08.12 환경 셋팅

--- src/main/java/com/boot/DevProjectApplication.java
+++ src/main/java/com/boot/DevProjectApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevProjectApplication.class, args);
+
+        SpringApplication.run(DevProjectApplication.class, args);
 	}
 
 }
